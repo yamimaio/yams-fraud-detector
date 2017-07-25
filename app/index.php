a21c1c75a26e88e80f28632f8ee99c1e5f1eb336
Yamila Maio <yamimaio@gmail.com>
2017-07-24 22:04:59 -0300

    Added logging.

--- app/index.php
+++ app/index.php
@@ -5,6 +5,8 @@
  * @author Yamila Maio  <yamimaio@gmail.com>
  */
 
+use Monolog\Handler\StreamHandler as StreamHandler;
+use Monolog\Logger as Logger;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -12,7 +14,20 @@ require_once '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
+//load dependencies as services
+$container = $app->getContainer();
+
+//add logger as a service
+$container['logger'] = function ($c) {
+    $logger = new Logger('fraud_logger');
+    $file_handler = new StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+//define API routes
 $app->get('/hello/{name}', function (Request $request, Response $response) {
+    $this->logger->addInfo("App is running. Hello route called");
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
 
