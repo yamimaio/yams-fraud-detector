3a40eaabc7d4dd25e94a66fb313bc2c759531eaa
Yamila Maio <yamimaio@gmail.com>
2017-07-26 22:06:16 -0300

    Implemented PaxLastName compliant with tests

--- src/FraudDetector/ScoringRules/PaxLastName.php
+++ src/FraudDetector/ScoringRules/PaxLastName.php
@@ -30,6 +30,50 @@ class PaxLastName extends ScoringRule
      */
     public function getScoring($order)
     {
+        //can't evaluate is only one passenger
+        if ($this->isOnlyPassenger($order)) {
+            return 0;
+        }
 
+        $lastNames = $this->getPassengerLastNameList($order);
+
+        //get number of times each lastname occurs
+        $lastNameCount = array_count_values($lastNames);
+
+        //check if any lastName is repeated
+        $matches = array_filter($lastNameCount, function ($count) {
+            return $count > 1;
+        });
+
+        //repeated lastNames means no risk
+        return $matches ? 0 : $this->scoring;
+    }
+
+    /**
+     * Indicates it order is for only one passenger.
+     *
+     * @param array $order Order to check
+     *
+     * @return bool
+     */
+    protected function isOnlyPassenger($order)
+    {
+        return $order['travel_ticket']['passengers'] == 1;
+    }
+
+    /**
+     * Returns list of passengers lastnames.
+     *
+     * @param array $order Order to check
+     *
+     * @return array
+     */
+    protected function getPassengerLastNameList($order)
+    {
+        $lastNames = array_column($order['travel_passengers'], 'last_name');
+        //normalize last names
+        return array_map(function ($lastName) {
+            return strtolower(trim($lastName));
+        }, $lastNames);
     }
 }
